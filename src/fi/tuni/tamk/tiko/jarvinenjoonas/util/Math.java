37ac4fccab5d35386c431b0fffe947d5391d7f54
JoonasJarvinen <joonasjarvin1@gmail.com>
2021-11-16 14:18:46 +0200

    fixed accidental leftover unused method

--- src/fi/tuni/tamk/tiko/jarvinenjoonas/util/Math.java
+++ src/fi/tuni/tamk/tiko/jarvinenjoonas/util/Math.java
@@ -16,28 +16,5 @@ public class Math {
     public static int getRandom(int min, int max) {
         return min + (int) (java.lang.Math.random() * ((max - min) + 1));
     }
-    /** 
-    *A method that removes a given index from a given int array
-    *
-    *@param original the int array that is to be updated
-    *@param index the index that is removed from original
-    *@return the new int array with one index removed
-    */
-    public static int[] removeIndex(int [] original, int index) {
-        original[index] = 0;
-        int [] updated = new int[original.length - 1];
-        int i = 0;
-        int j = 0;
-        while(i < updated.length - 1) {
-            if(j == index) {
-                j++;
-            } else {
-                updated[i] = original[j];
-                i++;
-                j++;
-            }
-        }
-        return updated;
-    }
 }
 
